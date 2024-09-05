00369a5d88e7f85eee138fff5cb62aeda94a96e6
Maciej Grzela <m.grzela@softcraft.global>
2024-09-05 16:21:26 +0200

    Spike webhook mock

---
+++ WebApplication1/Controllers/WebhookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[AllowAnonymous]
+public class WebhookController : Controller
+{
+    [HttpPost("post-webhook", Name = "PostWebhook")]
+    public async Task<IActionResult> PostWebHook([FromBody] object body)
+    {
+        var headers = HttpContext.Request.Headers;
+
+        return Ok(new
+        {
+            Body = body,
+            Signature = headers["x-spike-signature"]
+        });
+    }
+    
+    [HttpGet("get-webhook", Name = "GetWebhook")]
+    public async Task<IActionResult> GetWebhook([FromBody] object body)
+    {
+        var headers = HttpContext.Request.Headers;
+
+        return Ok(new
+        {
+            Body = body,
+            Signature = headers["x-spike-signature"]
+        });
+    }
+}
