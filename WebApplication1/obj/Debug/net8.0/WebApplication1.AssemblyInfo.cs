3863aead55bb4da4510ead6d9877bbd21f113b5d
Maciej Grzela <m.grzela@softcraft.global>
2024-09-05 16:35:52 +0200

    Dotnet 8

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f69e379362c9182884023a2af438ae1e73a97dbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0db1cfdff2b35f4bfcd89e2975e37984796cd898")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
